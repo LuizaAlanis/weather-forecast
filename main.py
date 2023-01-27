892bd87b4771bb9ba1de3dca911dedd2b300029f
Luiza <64799018+LuizaAlanis@users.noreply.github.com>
2023-01-26 23:06:08 -0300

    Update main.py

--- main.py
+++ main.py
@@ -2,17 +2,16 @@ import requests
 
 #input the city name
 city = input('input the city name')
-print(city)
 
-# or you can also hard-code the value
-# city = 'bhopal'
+weather(city)
 
-#Display the message!
-print('Displaying Weater report for: ' + city)
+def weather(city)
+  # display the message!
+  print('Displaying Weater report for: ' + city)
+  
+  # fetch the weater details
+  url = 'https://wttr.in/{}'.format(city)
+  res = requests.get(url)
 
-#fetch the weater details
-url = 'https://wttr.in/{}'.format(city)
-res = requests.get(url)
-
-#display the result!
-print(res.text)
+  #display the result!
+  print(res.text)
